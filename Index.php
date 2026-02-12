41b96d541d1ff506189ba24cc7aed57234884d85
Teddy Paz <pazteddy@gmail.com>
2026-02-12 14:19:47 -0400

    Refactor Index.php to return JSON response and add test.json for data retrieval

--- Index.php
+++ Index.php
@@ -2,7 +2,20 @@
 
 declare(strict_types=1);
 
-$method = $_SERVER["REQUEST_METHOD"];
-echo "Método HTTP recibido: $method\n";
-http_response_code(200); // OK
-echo "Servidor activo y funcionando\n";
+// $method = $_SERVER["REQUEST_METHOD"];
+// echo "Método HTTP recibido: $method\n";
+// http_response_code(200); // OK
+// echo "Servidor activo y funcionando\n";
+
+header("Content-Type: application/json");
+$response =
+    [
+        "status" => "success",
+        "message" => "API funcionando correctamente"
+    ];
+echo json_encode($response);
+
+$input = file_get_contents("test.json");
+$data = json_decode($input, true);
+
+echo json_encode($data);
